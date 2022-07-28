a9a93038dd8a72d8ffc892c96168613eaa8aebec
David Egri <david.egri95@gmail.com>
2022-07-27 17:01:42 -0700

    Adding queries

--- schema.sql
+++ schema.sql
@@ -36,13 +36,13 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-	dept_no varchar(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no varchar(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -51,8 +51,8 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
 -- Testing to see if columns were created.
-SELECT * FROM departments;
+SELECT * FROM titles;
